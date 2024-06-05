f88477371c31c11cbc2552dbc969f12c9888e653
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2024-06-05 00:51:10 -0500

    Get customer input to calculate and output volume

--- Main.java
+++ Main.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Please select a number for the type of calculation you want this program to perform\n" +
-                "1 - Volume Calculation \n 2 - Area Calculation");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("At the current moment, this software package only calculates the volume of a sphere,\n" +
+                " please enter the radius of the sphere (in meters) and this software will return the volume, or enter -1 to exit");
+        String input = scanner.nextLine();
+        double userNumber = 0;
+
+        try {
+            userNumber = Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid number next time.");
+            System.exit(1);
+        }
+
+        if(userNumber == -1){
+            System.exit(0);
+        }
+
+        System.out.println("User number is: " + userNumber);
+        Sphere sphere = new Sphere(userNumber);
+        double volume = sphere.calculateVolume();
+        System.out.println("The volume of your sphere is " + volume + " meters cubed");
 
     }
 }
