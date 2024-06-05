3154789a071474f5f9169aefb8e0ae1f4b24354c
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2024-06-04 23:40:36 -0500

    Add necessary files

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Please select a number for the type of calculation you want this program to perform\n" +
+                "1 - Volume Calculation \n 2 - Area Calculation");
+
+    }
+}
