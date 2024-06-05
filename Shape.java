eda71fc36075b5ead52904ed423da67030ff8b97
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2024-06-05 00:01:42 -0500

    Implement shape methods for Sphere class

--- Shape.java
+++ Shape.java
@@ -1,6 +1,6 @@
 public interface Shape {
 
-    void calculateVolume();
+    double calculateVolume();
 
-    void calculateArea();
+    double calculateArea();
 }
--- Sphere.java
+++ Sphere.java
@@ -1,4 +1,26 @@
 public class Sphere implements Shape{
 
+    private double radius;
 
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double calculateVolume() {
+        return (4.0/3.0) * Math.PI * Math.pow(radius, 3);
+    }
+
+    @Override
+    public double calculateArea() {
+        return 4 * Math.PI * radius * radius;
+    }
 }
